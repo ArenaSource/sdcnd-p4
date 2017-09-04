562d3a86083602b1d3a9b9afda5a5c235e6b66e6
Jose Luis Arenas <arenas@softblocks.io>
2017-09-04 19:11:24 +0100

    Add debug mode for Image fit and adjust

--- model.py
+++ model.py
@@ -46,7 +46,7 @@ class Camera(object):
         objp = np.zeros((np.prod(shape), 3), np.float32)
         objp[:, :2] = np.mgrid[0:shape[0], 0:shape[1]].T.reshape(-1, 2)
 
-        for file in glob.glob('camera_cal/*'):
+        for file in glob.glob(images):
             img = cv2.imread(file, 1)
             if img is None:
                 logging.warning('Unable to open {} image file for calibration'.format(file))
@@ -188,10 +188,11 @@ class Image(object):
         right = np.argmax(histogram[midpoint:]) + midpoint
         return left, right
 
-    def fit(self, windows=9):
+    def fit(self, debug=False):
 
-        # Create an output image to draw on and visualize the result
-        out_img = np.dstack((np.zeros_like(self.pixels), np.zeros_like(self.pixels), np.zeros_like(self.pixels))) * 255
+        if debug:
+            # Create an output image to draw on and visualize the result
+            out_img = np.dstack((self.pixels, self.pixels, self.pixels)) * 255
 
         leftx_base, rightx_base = self.peaks()
 
@@ -224,9 +225,10 @@ class Image(object):
             win_xright_low = rightx_current - margin
             win_xright_high = rightx_current + margin
 
-            # Draw the windows on the visualization image
-            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 4)
-            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 4)
+            if debug:
+                # Draw the windows on the visualization image
+                cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 4)
+                cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 4)
 
             # Identify the nonzero pixels in x and y within the window
             good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (
@@ -256,9 +258,22 @@ class Image(object):
         left_fit = np.polyfit(lefty, leftx, 2)
         right_fit = np.polyfit(righty, rightx, 2)
 
-        return left_fit, right_fit
+        if debug:
+            out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+            out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+            plt.imshow(out_img)
+            # Generate x and y values for plotting
+            y = np.linspace(0, self.shape[0] - 1, self.shape[0])
+            lx = left_fit[0] * y ** 2 + left_fit[1] * y + left_fit[2]
+            rx = right_fit[0] * y ** 2 + right_fit[1] * y + right_fit[2]
+            plt.plot(lx, y, color='yellow')
+            plt.plot(rx, y, color='yellow')
+            plt.xlim(0, 1280)
+            plt.ylim(720, 0)
 
-    def adjust(self, left_fit, right_fit):
+        return left_fit, right_fit,
+
+    def adjust(self, left_fit, right_fit, debug=False):
 
         binary_warped = self.pixels
 
@@ -290,30 +305,31 @@ class Image(object):
         left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
         right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
 
-        # Create an image to draw on and an image to show the selection window
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
-        window_img = np.zeros_like(out_img)
-        # Color in left and right line pixels
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        if debug:
+            # Create an image to draw on and an image to show the selection window
+            out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+            window_img = np.zeros_like(out_img)
+            # Color in left and right line pixels
+            out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+            out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-        # Generate a polygon to illustrate the search window area
-        # And recast the x and y points into usable format for cv2.fillPoly()
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
-        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
-        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
-        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+            # Generate a polygon to illustrate the search window area
+            # And recast the x and y points into usable format for cv2.fillPoly()
+            left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
+            left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
+            left_line_pts = np.hstack((left_line_window1, left_line_window2))
+            right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
+            right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
+            right_line_pts = np.hstack((right_line_window1, right_line_window2))
 
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
-        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
-        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-        plt.imshow(result)
-        plt.plot(left_fitx, ploty, color='yellow')
-        plt.plot(right_fitx, ploty, color='yellow')
-        plt.xlim(0, 1280)
+            # Draw the lane onto the warped blank image
+            cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+            cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+            result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+            plt.imshow(result)
+            plt.plot(left_fitx, ploty, color='yellow')
+            plt.plot(right_fitx, ploty, color='yellow')
+            plt.xlim(0, 1280)
 
         return left_fit, right_fit
 
@@ -322,7 +338,6 @@ class Image(object):
             plt.imshow(self.RGB.pixels, cmap=cmap)
         else:
             plt.imshow(self.pixels, cmap='gray')
-        plt.show()
 
 
 class Line(object):
